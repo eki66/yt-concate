67c89dee4913ff726d9c557d5f035a5d6e71e000
eki66 <soshieki@gmail.com>
2020-11-11 20:13:58 +0800

    Successfully download captions and save videos list to file

--- yt_concate/pipeline/steps/step.py
+++ yt_concate/pipeline/steps/step.py
@@ -1,6 +1,7 @@
 from abc import ABC
 from abc import abstractmethod
 
+
 class Step(ABC):
     def __init__(self):
         pass
@@ -8,6 +9,7 @@ class Step(ABC):
     @abstractmethod
     def process(self, data, inputs):
         pass
+
 
 class StepException(Exception):
     pass
